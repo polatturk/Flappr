4d09ece44793d61eac16bb9c5adba21cf7b47289
polat türk <polatturr52@gmail.com>
2025-09-01 10:30:28 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Flappr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
