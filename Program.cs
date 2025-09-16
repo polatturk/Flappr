9a50f4d742d2239494cc1a5d262c707547b8252b
polat türk <polatturr52@gmail.com>
2025-09-16 16:06:56 +0300

    Updated...

--- Program.cs
+++ Program.cs
@@ -95,7 +95,7 @@ namespace Flappr
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Login}/{id?}");
 
             app.Run();
         }
