d70f164d770ccfe084490cc9605c1204c7753418
polat türk <polatturr52@gmail.com>
2025-09-26 17:08:02 +0300

    Updated

---
+++ Helpers/TimeHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Flappr.Helpers
+{
+    public static class TimeHelper
+    {
+        public static string FormatTime(DateTime createdDate)
+        {
+            var now = DateTime.Now;
+            var diff = now - createdDate;
+
+            if (diff.TotalDays < 1)
+            {
+                int hours = (int)diff.TotalHours;
+                if (hours < 1)
+                {
+                    int minutes = (int)diff.TotalMinutes;
+                    return minutes <= 0 ? "şimdi" : $"{minutes} dk önce";
+                }
+                return $"{hours} s önce";
+            }
+            else if (diff.TotalDays < 7)
+            {
+                int days = (int)diff.TotalDays;
+                return $"{days} g önce";
+            }
+            else
+            {
+                return createdDate.ToString("d", CultureInfo.GetCultureInfo("tr"));
+            }
+        }
+    }
+}
